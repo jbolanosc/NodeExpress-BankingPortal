dd9c04025322130af81f5b8c74526ea0d658c4ae
jbolanosc <josue.carit@outlook.com>
2019-03-11 10:12:38 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index', { title: 'Index'}));
 
