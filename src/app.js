748ac566e8d4bd6bf3069e4f81e43ed74784afe5
jbolanosc <josue.carit@outlook.com>
2019-03-11 13:52:11 -0600

    module 3.2

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.post('/transfer', (req, res) => {
 
   fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
 
-  res.render('transfer', { message: 'transfer completed', account: accounts.credit});
+  res.render('transfer', { message: 'Transfer Completed', account: accounts.credit});
 });
 
 app.get('/payment', (req, res)=> res.render('payment', { account : accounts.credit}));
